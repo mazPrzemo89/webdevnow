4b5622649f267b09e1d647c3fd185ef7ff553f23
przem <maz89prz@gmail.com>
2020-10-02 21:46:49 +0100

    --

--- src/Components/Events and Input/EventsAndInput.tsx
+++ src/Components/Events and Input/EventsAndInput.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 
-const Home = () => {
+const EvsAndIntp = () => {
 
 
     const [value, setValue] = useState<string>('')
@@ -53,4 +53,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default EvsAndIntp
